3f956f52ec7cfe9474769a890736d5b6ad8f7413
Sammy Mohammed <sammymohammed00@gmail.com>
2023-03-11 15:37:56 -0800

    Adding QLearningModel to the tft_gym code

--- tft_gym/tft_gym.py
+++ tft_gym/tft_gym.py
@@ -1,4 +1,8 @@
 import numpy as np
+import math
+import random
+import pandas as pd
+
 
 import gymnasium as gym
 from gymnasium import spaces
@@ -59,3 +63,47 @@ class TFTBoardEnv(gym.Env):
         reward = (self._your_units - self._enemy_units * self._level) + self._gold - np.sqrt(gold_spent)
         self._gold -= gold_spent
         return self._get_obs, reward, terminated, False, None #leaving info as none
+
+class QLearningModel():
+    def __init__(self, states, actions, discount, lr):
+        self.states = states
+        self.actions = actions
+        self.discount = discount
+        self.Q = np.zeros((len(states), len(actions)))
+        self.lr = lr
+
+def update_step(model, s, a, r, s1):
+    model.Q[s, a] += model.lr * (r + model.discount*np.max(model.Q[s1, :]) - model.Q[s,a])
+    return model
+
+def greedy_policy(model, ep=.1):
+    # add the epsilon part of greedy epsilon
+    if random.random() <= ep:
+        return np.random.choice(model.Q, axis=-1)
+    return np.argmax(model.Q, axis=-1)
+
+def QLearning(file, nstates, nactions, discount, lr, output, iterations):
+    qlm = QLearningModel([0 for i in range(nstates)], [0 for i in range(nactions)], discount, lr)
+    df = pd.read_csv(file)
+    policy = None
+    s2 = 0
+    r2 = 0
+    a2 = 0
+    for i in range(iterations):
+        for index, row in df.iterrows():
+            s,a,r,sp = row
+            s -= 1
+            sp -= 1 
+            a -= 1
+            if s != s2:
+                qlm = update_step(qlm, s2, a2, r2, s)
+            s2 = s
+            a2 = a
+            r2 = r
+    policy = greedy_policy(qlm)
+    policy = policy + np.ones_like(policy)
+    print("sheesh")
+    policy.tofile(output + '.policy', sep="\n")
+    print(output)
+
+# QLearning("data/small.csv", 100, 4, .95, .2, 'small', 1)
