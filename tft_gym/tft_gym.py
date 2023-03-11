17614a5189abc8b44af43f990ca4148f1212244b
Sammy Mohammed <sammymohammed00@gmail.com>
2023-03-11 14:04:46 -0800

    Updating state to include enemy units and your own units, updating reward as well.

--- tft_gym/tft_gym.py
+++ tft_gym/tft_gym.py
@@ -14,14 +14,16 @@ class TFTBoardEnv(gym.Env):
             {
                 "hp": spaces.Discrete(100),
                 "gold": spaces.Discrete(200), # assume max gold is 200
-                "level": spaces.Discrete(9)
+                "level": spaces.Discrete(9),
+                "your_units": spaces.Discrete(10),
+                "enemy_units": spaces.Discrete(10),
             }
         )
 
         # We have 4 actions, corresponding to "reroll", "increase level", "open", "do nothing"
         self.action_space = spaces.Discrete(4)
     def _get_obs(self):
-        return {"hp": self._hp, "level": self._level, "gold": self._gold}
+        return {"hp": self._hp, "level": self._level, "gold": self._gold, "units": self._your_units, "enemy_units": self._enemy_units}
     
     def reset(self, seed=None, options=None):
         # We need the following line to seed self.np_random
@@ -30,18 +32,21 @@ class TFTBoardEnv(gym.Env):
         self._hp = 100
         self._level = 1
         self._gold = 0
+        self._your_units = 1
+        self._enemy_units = 1
 
         observation = self._get_obs()
         return observation, None     
     
     def step(self, action):
         terminated = False
+        gold_spent = 0
         if action == 0:
             if np.random.random() < .33: # we can change this for final version, assuming 1 guy in lobby high rolled
                self._hp -= 5
-            self._gold -= 10
+            gold_spent -= 10
         if action == 1:
-            self._gold -= 20
+            gold_spent -= 20
         if action == 2:
             self._gold += 10
             self._hp -= 10
@@ -51,5 +56,6 @@ class TFTBoardEnv(gym.Env):
                 self._hp -= 5
         if (self._hp <= 0):
             terminated = True
-        reward = self._hp + self._gold
+        reward = (self._your_units - self._enemy_units * self._level) + self._gold - np.sqrt(gold_spent)
+        self._gold -= gold_spent
         return self._get_obs, reward, terminated, False, None #leaving info as none
